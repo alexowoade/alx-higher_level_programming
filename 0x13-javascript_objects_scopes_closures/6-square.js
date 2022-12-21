729492b76c0b83628be4d5f18330bde4d88f26bb
alexowoade <alexowoade@gmail.com>
2022-12-21 12:17:51 -0800

    alex

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,37 @@
+
+NonsoGodson
+/
+alx-higher_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-higher_level_programming/0x13-javascript_objects_scopes_closures/6-square.js
+@NonsoGodson
+NonsoGodson commit
+ 1 contributor
+Executable File  20 lines (18 sloc)  559 Bytes
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+
+/**
+ * Represents a Rectangle with 4 equal sides.
+ */
+class Square extends SquareParent {
+  /**
+   * Prints this Square with the given character, otherwise 'X'.
+   * @param {String} c The character to print this Square with.
+   */
+  charPrint (c) {
+    const pen = c === undefined ? 'X' : c;
+    const row = new Array(this.width).fill(pen, 0, this.width);
+    const rows = new Array(this.height).fill(row.join(''), 0, this.height);
+    console.log(rows.join('\n'));
+  }
+}
+
+module.exports = Square;
