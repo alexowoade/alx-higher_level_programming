0d922dfcb8e2d4d7db34df4d8be7746c880b69da
alexowoade <alexowoade@gmail.com>
2023-02-11 18:16:08 +0000

    alex

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-# Sends a POST request to a given URL, and displays the body of the response
-curl -sL -X POST -H 'Content-Type: application/json' -d "$([ -f "$2" ] && cat "$2")" "$1"
+
+# Validate the JSON payload
+function validate_json() {
+    jq . "$1" > /dev/null 2>&1
+    return $?
+}
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 URL JSON_FILE"
+    exit 1
+fi
+
+URL="$1"
+JSON_FILE="$2"
+
+# Validate the JSON payload
+validate_json "$JSON_FILE"
+if [ $? -ne 0 ]; then
+    echo "Not a valid JSON"
+    exit 1
+fi
+
+# Send the POST request with the JSON payload
+curl -sX POST -H "Content-Type: application/json" -d @"$JSON_FILE" "$URL"
+echo "Valid JSON"
 
